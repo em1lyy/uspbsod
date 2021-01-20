d6e57700d9f61c485c4493e952f6f7157496b039
Emily <36363495+jonasjaguar@users.noreply.github.com>
2021-01-20 21:43:50 +0100

    recompile (fix?)

--- ntraise/ntraise.c
+++ ntraise/ntraise.c
@@ -4,11 +4,11 @@ int main(void) {
     /* don't ask me. the types are from those windows fuckers */
     PBOOLEAN retval1 = malloc(sizeof(BOOLEAN));
     PULONG retval2 = malloc(sizeof(ULONG));
-    PULONG_PTR intPtrZero = malloc(sizeof(PULONG));
-    *intPtrZero = 0;
+    INT_PTR intPtrZero = malloc(sizeof(INT_PTR));
+    intPtrZero = 0;
 
     RtlAdjustPrivilege(19, 1, 0, retval1);
-    NtRaiseHardError(0xc0000420, 0, 0, intPtrZero, 6, retval2);
+    NtRaiseHardError(0xc0000022, 0, 0, intPtrZero, 6, retval2);
 
     return -1;
 }
